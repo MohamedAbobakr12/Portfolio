ffb9305b5d814025a0493f80dee5addcd4f35bd6
Dev.Mohamed <102235955+MohamedAbobakr12@users.noreply.github.com>
2024-09-29 22:39:26 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
                     <div className="flex items-center gap-3">
                         <figure className="img-box w-9 h-9 rounded-lg">
                             <img 
-                                src="public/imgs/avatar-1.jpg" 
+                                src="imgs/avatar-1.jpg" 
                                 width={40} 
                                 height={40} 
                                 alt="Henry clark portrait" 
@@ -60,7 +60,7 @@ const Hero = () => {
                 <div className="hidden lg:block">
                     <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
                         <img 
-                            src="public/imgs/hero-banner.png" 
+                            src="imgs/hero-banner.png" 
                             width={656}
                             height={800}
                             alt="Henry Clark" 
