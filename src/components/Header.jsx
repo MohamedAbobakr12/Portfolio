51ec03de69e29fa96c30c27059ad28e0c051b6e3
Dev.Mohamed <102235955+MohamedAbobakr12@users.noreply.github.com>
2024-09-29 22:38:39 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
                         className="logo"
                     >
                         <img 
-                            src="public/imgs/logo.svg" 
+                            src="imgs/logo.svg" 
                             width={40}
                             height={40}
                             alt="Henry Clark" 
